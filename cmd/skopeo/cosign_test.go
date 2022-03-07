d2fe9a43921330e953d49e38f315729fba910093
Miloslav Trmač <mitr@redhat.com>
2022-03-08 00:52:21 +0100

    WIP Fulcio verification support
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- cmd/skopeo/cosign_test.go
+++ cmd/skopeo/cosign_test.go
@@ -102,7 +102,7 @@ func TestCosignStandaloneVerify(t *testing.T) {
 
 // FIXME: Also c/image policy verification for interoperability, both ways.
 
-func TestCosignRekorVerify(t *testing.T) {
+func TestCosignStandaloneRekorVerifyKeyOnly(t *testing.T) {
 	ensureTestImage(t)
 
 	keys := generateKeys(t)
@@ -142,3 +142,68 @@ func TestCosignRekorVerify(t *testing.T) {
 		"--rekor-set", setPath, filepath.Join(imageDir, "manifest.json"),
 		filepath.Join(sigImageDir, layers[0].Digest.Encoded()), sigPath)
 }
+
+func TestCosignFulcioRekorVerify(t *testing.T) {
+	ensureTestImage(t)
+
+	testRepo := fmt.Sprintf("%s/test-repo-%d", cosignTempRegistry, rand.NewSource(time.Now().Unix()).Int63())
+	testImage := testRepo + "/alpine:3.10.2"
+	runAndLogSkopeo(t, "--insecure-policy", "copy", "--dest-tls-verify=false", "--src-tls-verify=false", "--all",
+		"docker://"+cosignPristineTestImage, "docker://"+testImage)
+
+	dir := t.TempDir()
+	sigPath := filepath.Join(dir, "sig")
+	setPath := filepath.Join(dir, "set")
+	certPath := filepath.Join(dir, "cert")
+	chainPath := filepath.Join(dir, "chain")
+
+	func() {
+		tty, err := os.OpenFile("/dev/tty", os.O_RDWR, 0)
+		if err != nil {
+			t.Skipf("No TTY for signing")
+		}
+		require.NoError(t, err)
+		defer tty.Close()
+		cmd := exec.Command("cosign", "sign", "--tlog-upload", "--output-signature", sigPath, "--output-certificate", certPath,
+			"--yes", // Skip privacy prompt
+			testImage)
+		cmd.Stdin = tty
+		cmd.Stdout = tty
+		cmd.Stderr = tty
+		err = cmd.Run()
+		require.NoError(t, err)
+	}()
+
+	imageDir := t.TempDir()
+	runAndLogSkopeo(t, "--insecure-policy", "copy", "--all", "--src-tls-verify=false", "docker://"+testImage, "dir:"+imageDir)
+
+	sigImageDir := t.TempDir()
+	runAndLogSkopeo(t, "--insecure-policy", "copy", "--all", "--src-tls-verify=false", "docker://"+testRepo+"/alpine:sha256-fa93b01658e3a5a1686dc3ae55f170d8de487006fb53a28efcd12ab0710a2e5f.sig", "dir:"+sigImageDir)
+	sigRef, err := directory.NewReference(sigImageDir)
+	require.NoError(t, err)
+	sigSrc, err := sigRef.NewImageSource(context.Background(), nil)
+	require.NoError(t, err)
+	image, err := image.FromSource(context.Background(), nil, sigSrc)
+	require.NoError(t, err)
+	layers := image.LayerInfos()
+	require.NotEmpty(t, layers)
+	setBlob, ok := layers[0].Annotations["dev.sigstore.cosign/bundle"]
+	require.True(t, ok)
+	err = os.WriteFile(setPath, []byte(setBlob), 0600)
+	require.NoError(t, err)
+	chainBlob, ok := layers[0].Annotations["dev.sigstore.cosign/chain"]
+	require.True(t, ok)
+	err = os.WriteFile(chainPath, []byte(chainBlob), 0600)
+	require.NoError(t, err)
+
+	runAndLogSkopeo(t, "cosign-standalone-verify",
+		"--fulcio", "fixtures/fulcio_v1.crt.pem",
+		"--fulcio-issuer", "https://github.com/login/oauth",
+		"--fulcio-email", "mitr@redhat.com",
+		"--require-rekor=true",
+		"--embedded-cert", certPath, "--cert-chain", chainPath, "--rekor-set", setPath,
+		filepath.Join(imageDir, "manifest.json"),
+		filepath.Join(sigImageDir, layers[0].Digest.Encoded()), sigPath)
+}
+
+// FIXME: Test, or remove, the --ca mode
